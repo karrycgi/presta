511fb7551b19069e0bff5b5198ace4cf37f7377e
Martin Karry <martin.karry@cgi.com>
2025-10-16 13:56:19 +0200

    Added ModuleFrontendController working

--- demopay/controllers/front/pay.php
+++ demopay/controllers/front/pay.php
@@ -3,6 +3,8 @@
 class DemoPayPayModuleFrontController extends ModuleFrontController {
     public function initContent() {
         parent::initContent();
+        //dump($this);
+        //$this->setTemplate("module:demopay/views/templates/front/test.tpl");
         Tools::redirect('https://www.google.com');
     }
 }
