7254e42fa5b1900ad0d61405d94f0eaf093ab291
Martin Karry <martin.karry@cgi.com>
2025-10-16 14:05:12 +0200

    Added preparePaymentLink

--- demopay/controllers/front/pay.php
+++ demopay/controllers/front/pay.php
@@ -1,10 +1,14 @@
 <?php
 
 class DemoPayPayModuleFrontController extends ModuleFrontController {
+    private function preparePaymentLink($type, $total) {
+        return "http://www.heise.de";
+    }
     public function initContent() {
         parent::initContent();
+        $type = $_GET["type"];
         //dump($this);
         //$this->setTemplate("module:demopay/views/templates/front/test.tpl");
-        Tools::redirect('https://www.google.com');
+        Tools::redirect($this->preparePaymentLink($type, 100));
     }
 }
