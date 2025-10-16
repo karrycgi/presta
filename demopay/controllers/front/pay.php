ad8033aac435184dd26aac35d59bc6cc155dc8b8
Martin Karry <martin.karry@cgi.com>
2025-10-16 13:44:25 +0200

    Fixxed controller

--- demopay/controllers/front/pay.php
+++ demopay/controllers/front/pay.php
@@ -1,6 +1,6 @@
 <?php
 
-class DemoPayPayFrontController extends ModuleFrontController {
+class DemoPayPayModuleFrontController extends ModuleFrontController {
     public function initContent() {
         parent::initContent();
         Tools::redirect('https://www.google.com');
