e41a2e5780aaf22e13797d7c1b67193cc58ec721
Martin Karry <martin.karry@cgi.com>
2025-10-27 10:27:16 +0100

    Added simple implementation for checkout

--- demopay/controllers/front/success.php
+++ demopay/controllers/front/success.php
@@ -1,7 +1,39 @@
 <?php
+require_once dirname(__FILE__) . '/../../vendor/autoload.php';
 class DemoPaySuccessModuleFrontController extends ModuleFrontController {
+    public $module;
+    private $handler;
+
+    public function init() {
+        parent::init();
+        $this->handler = new CheckoutRequestHandler(
+            Configuration::get('DEMOPAY_STORE_ID'),
+            Configuration::get('DEMOPAY_API_KEY'),
+            Configuration::get('DEMOPAY_SECRET')
+        );  
+    }
     public function initContent() {
         parent::initContent();
-        Tools::redirect('https://www.whitescreen.online');
+
+        $customer = new Customer($this->context->cart->id_customer);
+
+        $this->module->validateOrder(
+            (int) $this->context->cart->id,
+            (int) Configuration::get("PS_OS_PAYMENT"),
+            (float) $this->context->cart->getOrderTotal(true, Cart::BOTH),
+            "DemoPay Test!!!"
+        );
+        
+        Tools::redirect($this->context->link->getPageLink(
+            'order-confirmation',
+            false,
+            (int) $this->context->language->id,
+            [
+                'id_cart' => (int) $this->context->cart->id,
+                'id_module' => (int) $this->module->id,
+                'id_order' => (int) $this->module->currentOrder,
+                'key' => $customer->secure_key,
+            ]
+        ));
     }
 }
