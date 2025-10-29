efe0a72244efa1489d4c507f7b103c8d4cb2e090
Martin Karry <martin.karry@cgi.com>
2025-10-29 10:47:16 +0100

    Regorganized code and fixed serialization problem

--- demopay/src/lib/CheckoutRequestHandler.php
+++ demopay/src/lib/CheckoutRequestHandler.php
@@ -1,49 +1,15 @@
 <?php
 
-class CheckoutRequestHandler
+require_once dirname(__FILE__) . '/../../vendor/autoload.php';
+class CheckoutRequestHandler extends RequestHandler
 {
-    public function __construct(private string $storeId, private string $apiKey, private string $secret)
+    private function prepareCreateCheckoutRequestBody(Cart $cart, string $successUrl, string $failureUrl, string $clientRequestId): string
     {
-    }
+        ini_set( 'serialize_precision', -1 );
 
-    private function getCheckoutUri()
-    {
-        if (Configuration::get(DemoPay::DEMO_PAY_SANDBOX_KEY) === 'FALSE') {
-            return 'https://prod.emea.api.fiservapps.com/exp/v1/checkouts';
-        }
+        $total = $cart->getCartTotalPrice();
 
-        return 'https://prod.emea.api.fiservapps.com/sandbox/exp/v1/checkouts';
-    }
-
-    private function sign(string $requestId, int $time, string $body)
-    {
-        $token = $this->apiKey . $requestId . $time . $body;
-        return base64_encode(hash_hmac('sha256', $token, strval($this->secret), true));
-    }
-
-    private static function generateUuid(): string
-    {
-        $out = bin2hex(random_bytes(18));
-
-        $out[8] = "-";
-        $out[13] = "-";
-        $out[18] = "-";
-        $out[23] = "-";
-        $out[14] = "4";
-        $out[19] = ["8", "9", "a", "b"][random_int(0, 3)];
-
-        return $out;
-    }
-
-    public function createCheckout(Cart $cart, string $successUrl, string $failureUrl)
-    {
-        $time = intval(microtime(true) * 1000);
-
-        $total = $cart->getTotalShippingCost();
-
-        $clientRequestId = CheckoutRequestHandler::generateUuid();
-
-        $requestBody = json_encode([
+        $json = json_encode([
             "storeId" => $this->storeId,
             "transactionOrigin" => "ECOM",
             "transactionType" => "SALE",
@@ -56,11 +22,22 @@ class CheckoutRequestHandler
             ],
             "checkoutSettings" => [
                 "redirectBackUrls" => [
-                    "successUrl" => $successUrl."?id=".$cart->id."&uuid=".$clientRequestId,
-                    "failureUrl" => $failureUrl."?id=".$cart->id."&uuid=".$clientRequestId
+                    "successUrl" => $successUrl . "?id=" . $cart->id . "&uuid=" . $clientRequestId,
+                    "failureUrl" => $failureUrl . "?id=" . $cart->id . "&uuid=" . $clientRequestId
                 ]
             ]
         ]);
+
+        return $json;
+    }
+
+    public function createCheckout(Cart $cart, string $successUrl, string $failureUrl)
+    {
+        $time = intval(microtime(true) * 1000);
+
+        $clientRequestId = CheckoutRequestHandler::generateUuid();
+
+        $requestBody = $this->prepareCreateCheckoutRequestBody($cart, $successUrl, $failureUrl, $clientRequestId);
 
         $messageSignature = $this->sign($clientRequestId, $time, $requestBody);
 
@@ -82,10 +59,6 @@ class CheckoutRequestHandler
     }
 
     public function checkoutStatus(string $checkoutId)
-    {
-
-    }
-    public function refund($checkoutId, $total)
     {
 
     }
---
+++ demopay/src/lib/RequestHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+class RequestHandler {
+    public function __construct(protected string $storeId, protected string $apiKey, protected string $secret)
+    {
+    }
+    protected function getCheckoutUri()
+    {
+        if (Configuration::get(DemoPay::DEMO_PAY_SANDBOX_KEY) === 'FALSE') {
+            return 'https://prod.emea.api.fiservapps.com/exp/v1/checkouts';
+        }
+
+        return 'https://prod.emea.api.fiservapps.com/sandbox/exp/v1/checkouts';
+    }
+
+    protected function sign(string $requestId, int $time, string $body)
+    {
+        $token = $this->apiKey . $requestId . $time . $body;
+        return base64_encode(hash_hmac('sha256', $token, strval($this->secret), true));
+    }
+
+    protected static function generateUuid(): string
+    {
+        $out = bin2hex(random_bytes(18));
+
+        $out[8] = "-";
+        $out[13] = "-";
+        $out[18] = "-";
+        $out[23] = "-";
+        $out[14] = "4";
+        $out[19] = ["8", "9", "a", "b"][random_int(0, 3)];
+
+        return $out;
+    }
+}
